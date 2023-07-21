c81b69ccc35d409d694e580c9d11cb68832a2b73
김영석 <keinetwork@gmail.com>
2023-07-21 16:50:40 +0900

    Project Start

---
+++ src/main/java/com/winters/be/BeWinterscomeApplication.java
@@ -0,0 +1,13 @@
+package com.winters.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWinterscomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeWinterscomeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/winters/be/HomeController.java
@@ -0,0 +1,13 @@
+package com.winters.be;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("home: ");
+		return "/index.html";
+	}
+}
