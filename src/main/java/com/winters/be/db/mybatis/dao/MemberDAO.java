dcb1a9c733056c9f7e95cdb3aa1902d82170d594
김영석 <keinetwork@gmail.com>
2023-08-27 16:27:19 +0900

    mybatis 설정

--- src/main/java/com/winters/be/db/mybatis/dao/MemberDAO.java
+++ src/main/java/com/winters/be/db/mybatis/dao/MemberDAO.java
@@ -2,9 +2,21 @@ package com.winters.be.db.mybatis.dao;
 
 import com.winters.be.db.mybatis.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 @Mapper
 public interface MemberDAO {
-    public List<MemberVO> getMemberList();
+    List<MemberVO> getMemberList();
+
+
+    @Select("SELECT * FROM member")
+    List<MemberVO> findAll();
+    @Select("SELECT userid as userId FROM member WHERE userid = #{userid} and password =#{password}")
+    MemberVO findByUserIdAndPassword(@Param("userid") String userid, @Param("password") String password);
+
+//    @Insert("INSERT INTO user(name, part) VALUES(#{name}, #{part}")
+//    @Options(useGeneratedKeys = true, keyProperty = "userIdx")
+//    int save(@Param("user") final User user);
 }
